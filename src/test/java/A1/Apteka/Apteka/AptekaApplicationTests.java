6c1f63d1b9677db568100501c7019f9302ce0193
FromJuniorToJunior <j.bondaruk98@gmail.com>
2022-03-15 13:15:14 +0100

    initial commit

---
+++ src/test/java/A1/Apteka/Apteka/AptekaApplicationTests.java
@@ -0,0 +1,13 @@
+package A1.Apteka.Apteka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
