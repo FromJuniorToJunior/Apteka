9a0535ad82e94aa7ab00f272318f96f9b41b917b
jaroslaw.bondaruk <jaroslaw.bondaruk@computerplus.com.pl>
2022-04-04 20:16:01 +0200

    CreateAddress endpoint, basic controller unit test.

--- src/main/java/A1/Apteka/Apteka/Repository/AddressRepository.java
+++ src/main/java/A1/Apteka/Apteka/Repository/AddressRepository.java
@@ -3,6 +3,12 @@ package A1.Apteka.Apteka.Repository;
 import A1.Apteka.Apteka.Model.Address;
 import A1.Apteka.Apteka.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address,Long> {
+    @Query("SELECT a FROM Address a WHERE a.address_id = ?1")
+    Address znajdz(Long id);
+
 }
